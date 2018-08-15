c8f5c3a53250b26b072953b285b534479fdd7127
Nathan VanOrman <nathangvo@gmail.com>
2018-08-14 19:11:01 -0600

    added redux-form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import SigninForm from './signinForm';
 
 class Signin extends Component {
   render() {
     return (
       <div className='sign-in'>
-        ...signin
+        <SigninForm/>
       </div>
     )
   }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export function Header() {
+  return(
+      <div className='header'>
+          <h1 className='header__title'>Welcome to HOA Manager!</h1>
+          <p className='header__subtitle'>Please login to continue</p>
+      </div>
+  )
+}
